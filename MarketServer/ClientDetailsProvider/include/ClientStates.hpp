d8db2d65215f4a290b10ab569eaca747dac47f21
maxime corporation <maxime.himeno@epita.fr>
2026-03-23 20:32:22 +0100

    some more files

---
+++ MarketServer/ClientDetailsProvider/include/ClientStates.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace ClientDetailsProvider
+{
+    class ClientStates
+    {
+    private:
+    };
+} // namespace ClientDetailsProvider
--- MarketServer/SocketGateway/include/EpollServer.ipp
+++ MarketServer/SocketGateway/include/EpollServer.ipp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <EpollServer.hpp>
 
 namespace Gateways
